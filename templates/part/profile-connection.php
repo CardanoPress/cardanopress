5cd32e49571c93e4762babf9085e09a6fd41357e
kermage <genealyson.torcende@gmail.com>
2021-12-08 14:49:17 +0800

    optimized calls

--- templates/part/profile-connection.php
+++ templates/part/profile-connection.php
@@ -1,30 +1,36 @@
+<?php
+
+$userProfile = cardanoPress()->userProfile();
+
+?>
+
 <h3>
     Account
 
-    <div class="ml-3 inline-block">
-        <?php if (! cardanoPress()->userProfile()->connectedStake()) : ?>
+    <span class="ml-3">
+        <?php if (! $userProfile->connectedStake()) : ?>
             <button type="button" @click="showModal = true">Reconnect</button>
         <?php else : ?>
             <?php cardanoPress()->template('part/asset-sync'); ?>
         <?php endif; ?>
-    </div>
+    </span>
 </h3>
 
 <table class="w-full table-auto border-collapse">
     <tbody>
         <tr>
             <th>Network</th>
-            <td><?php echo cardanoPress()->userProfile()->connectedNetwork(); ?></td>
+            <td><?php echo $userProfile->connectedNetwork(); ?></td>
         </tr>
 
         <tr>
             <th>Wallet</th>
-            <td><?php echo cardanoPress()->userProfile()->connectedWallet(); ?></td>
+            <td><?php echo $userProfile->connectedWallet(); ?></td>
         </tr>
 
         <tr>
             <th>Stake</th>
-            <td><?php echo cardanoPress()->userProfile()->connectedStake(); ?></td>
+            <td><?php echo $userProfile->connectedStake(); ?></td>
         </tr>
     </tbody>
 </table>
