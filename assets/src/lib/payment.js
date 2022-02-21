688cab4da8135be85752046bb75408cf35552b65
kermage <genealyson.torcende@gmail.com>
2022-02-21 18:02:40 +0800

    custom payment flow for typhon

--- assets/src/lib/payment.js
+++ assets/src/lib/payment.js
@@ -4,13 +4,39 @@ import Extensions from './extensions'
 import Extension from './extension'
 
 export const handlePayment = async (lovelaceValue, payee) => {
-    const walletObject = await Extensions.getWallet(localStorage.getItem('_x_connectedExtension'))
+    const connectedExtension = localStorage.getItem('_x_connectedExtension')
+    const walletObject = await Extensions.getWallet(connectedExtension)
     const Wallet = new Extension(walletObject)
     const network = await Wallet.getNetwork()
     const response = await getProtocol(network)
 
     if (!response.success) {
         return response
+    }
+
+    if ('Typhon' === connectedExtension) {
+        try {
+            const response = await walletObject.paymentTransaction({
+                outputs: [{
+                    address: payee,
+                    amount: lovelaceValue,
+                }],
+            })
+
+            if (response.status) {
+                return await saveWalletTx(network, await Wallet.getChangeAddress(), response.data.transactionId)
+            }
+
+            return {
+                success: false,
+                data: response.reason,
+            }
+        } catch (error) {
+            return {
+                success: false,
+                data: error,
+            }
+        }
     }
 
     const protocolParameters = response.data
