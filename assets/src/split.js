43519bf2625b0fb6923bb22c44d2886da10e9793
kermage <genealyson.torcende@gmail.com>
2022-06-06 10:56:31 +0800

    also Split to follow the Payment

--- assets/src/split.js
+++ assets/src/split.js
@@ -9,29 +9,10 @@ window.addEventListener('alpine:init', () => {
     const Alpine = window.Alpine || {}
 
     Alpine.data('splitForm', () => ({
-        isVerified: false,
         isProcessing: false,
-        remainingBalance: 0,
         percentage: 0,
         address: '',
         transactionHash: '',
-
-        async init() {
-            window.addEventListener('load', async () => {
-                if (getConnectedExtension()) {
-                    try {
-                        const Wallet = await getConnectedWallet()
-                        this.remainingBalance = parseInt(await Wallet.getBalance()) - parseInt(adaToLovelace(this.$root.dataset.fee))
-                    } catch (error) {
-                        addNotice({ type: 'error', text: error })
-                    }
-                }
-            })
-
-            window.addEventListener('cardanoPress:recaptcha', async (event) => {
-                this.isVerified = event.detail
-            }, { once: true })
-        },
 
         isReady() {
             return !!(this.isVerified && !this.isProcessing && this.percentage && this.address)
