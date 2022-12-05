c20ed6d2347ba11d30f62f3dddc042b6749e7e21
kermage <genealyson.torcende@gmail.com>
2022-12-05 15:48:06 +0800

    bump version to 0.39.0

--- cardanopress.php
+++ cardanopress.php
@@ -6,7 +6,7 @@
  * Author:      CardanoPress
  * Author URI:  https://cardanopress.io
  * Description: Core plugin for the suite of CardanoPress plugins
- * Version:     0.38.0
+ * Version:     0.39.0
  * License:     GPL-2.0-only
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  *
