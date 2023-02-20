13bafe8cd761c915ddf325fa496e1b98046683ce
kermage <genealyson.torcende@gmail.com>
2023-02-20 13:49:09 +0800

    customizable sanitization messages

--- src/Actions/Sanitization.php
+++ src/Actions/Sanitization.php
@@ -12,12 +12,33 @@ use CardanoPress\Clients\BlockfrostClient;
 // phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
 class Sanitization
 {
+    protected function customizableMessages()
+    {
+        return [
+            'query_network' => __('Invalid query network', 'cardanopress'),
+            'wallet_address' => __('Invalid wallet address', 'cardanopress'),
+            'stake_address' => __('Invalid stake address', 'cardanopress'),
+            'reward_address' => __('Invalid reward address', 'cardanopress'),
+            'pool_id' => __('Invalid pool id', 'cardanopress'),
+            'transaction_action' => __('Invalid transaction action', 'cardanopress'),
+            'transaction_hash' => __('Invalid transaction hash', 'cardanopress'),
+            'ada_handle' => __('Invalid ada handle', 'cardanopress'),
+        ];
+    }
+
+    public function getMessage(string $type): string
+    {
+        $messages = apply_filters('cardanopress_sanitization_messages', $this->customizableMessages());
+
+        return $messages[$type] ?? '';
+    }
+
     public function query_network(): string
     {
         $value = sanitize_key($_POST['query_network']);
 
         if (! array_key_exists($value, BlockfrostClient::ENDPOINT)) {
-            wp_send_json_error(__('Invalid query network', 'cardanopress'));
+            wp_send_json_error($this->getMessage('query_network'));
         }
 
         return $value;
@@ -28,7 +49,7 @@ class Sanitization
         $value = sanitize_text_field($_POST['wallet_address']);
 
         if ('' === $value || (0 !== strpos($value, 'addr1') && 0 !== strpos($value, 'addr_test1'))) {
-            wp_send_json_error(__('Invalid wallet address', 'cardanopress'));
+            wp_send_json_error($this->getMessage('wallet_address'));
         }
 
         return $value;
@@ -39,7 +60,7 @@ class Sanitization
         $value = sanitize_text_field($_POST['stake_address']);
 
         if ('' === $value || (0 !== strpos($value, 'stake1') && 0 !== strpos($value, 'stake_test1'))) {
-            wp_send_json_error(__('Invalid stake address', 'cardanopress'));
+            wp_send_json_error($this->getMessage('stake_address'));
         }
 
         return $value;
@@ -50,7 +71,7 @@ class Sanitization
         $value = sanitize_text_field($_POST['reward_address']);
 
         if ('' === $value || (0 !== strpos($value, 'stake1') && 0 !== strpos($value, 'stake_test1'))) {
-            wp_send_json_error(__('Invalid reward address', 'cardanopress'));
+            wp_send_json_error($this->getMessage('reward_address'));
         }
 
         return $value;
@@ -61,7 +82,7 @@ class Sanitization
         $value = sanitize_text_field($_POST['pool_id']);
 
         if ('' === $value || 0 !== strpos($value, 'pool1')) {
-            wp_send_json_error(__('Invalid pool id', 'cardanopress'));
+            wp_send_json_error($this->getMessage('pool_id'));
         }
 
         return $value;
@@ -72,7 +93,7 @@ class Sanitization
         $value = sanitize_key($_POST['transaction_action']);
 
         if (! in_array($value, ['payment', 'delegation'], true)) {
-            wp_send_json_error(__('Invalid transaction action', 'cardanopress'));
+            wp_send_json_error($this->getMessage('transaction_action'));
         }
 
         return $value;
@@ -83,7 +104,7 @@ class Sanitization
         $value = sanitize_key($_POST['transaction_hash']);
 
         if (! ctype_xdigit($value) || 64 !== strlen($value)) {
-            wp_send_json_error(__('Invalid transaction hash', 'cardanopress'));
+            wp_send_json_error($this->getMessage('transaction_hash'));
         }
 
         return $value;
@@ -94,7 +115,7 @@ class Sanitization
         $value = sanitize_text_field($_POST['ada_handle']);
 
         if (empty($value)) {
-            wp_send_json_error(__('Invalid ada handle', 'cardanopress'));
+            wp_send_json_error($this->getMessage('ada_handle'));
         }
 
         return $value;
