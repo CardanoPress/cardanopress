1f4b707adcb77ef9e6961ef48b67eddb1eb23fd1
kermage <genealyson.torcende@gmail.com>
2023-05-04 15:39:40 +0800

    update template for already delegated account

--- templates/pool-delegation.php
+++ templates/pool-delegation.php
@@ -9,6 +9,9 @@
  * @since   0.1.0
  */
 
+$accountInfo = cardanoPress()->userProfile()->getAccountInfo();
+$delegationPool = cardanoPress()->delegationPool();
+
 ?>
 
 <div x-data="poolDelegation">
@@ -20,11 +23,17 @@
         <?php cardanoPress()->template('part/delegation-connect'); ?>
     </div>
 
-    <div class="py-6">
-        <?php cardanoPress()->template('part/delegation-process'); ?>
-    </div>
+    <?php if ($accountInfo['active'] && $accountInfo['pool_id'] === $delegationPool['pool_id']) : ?>
+        <div class="py-6">
+            <button type='button' disabled='true'>Delegated</button>
+        </div>
+    <?php else : ?>
+        <div class="py-6">
+            <?php cardanoPress()->template('part/delegation-process'); ?>
+        </div>
 
-    <div class="py-6">
-        <?php cardanoPress()->template('part/delegation-result'); ?>
-    </div>
+        <div class="py-6">
+            <?php cardanoPress()->template('part/delegation-result'); ?>
+        </div>
+    <?php endif; ?>
 </div>
